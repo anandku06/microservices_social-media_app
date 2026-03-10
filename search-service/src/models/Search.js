6fae049d3b781b33ac1645af2bb5797a91546aa7
Anand Kumar <rishavgupta163@gmail.com>
2026-03-10 22:20:35 +0530

    feat: add SearchPost model and RabbitMQ utility for event consumption

---
+++ search-service/src/models/Search.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const searchPostSchema = new mongoose.Schema(
+  {
+    postId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+searchPostSchema.index({ content: "text" });
+searchPostSchema.index({ createdAt: -1 }); // for sorting by creation date
+
+const SearchPost = mongoose.model("SearchPost", searchPostSchema);
+module.exports = SearchPost;
